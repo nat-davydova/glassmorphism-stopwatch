ca27991f719f2c0ac4a0128f165f9b96808a44b6
nat-davydova <natti.davydova@gmail.com>
2023-12-12 17:37:12 +0300

    feat: add basic body styles

--- src/components/DemoHeader/DemoHeader.tsx
+++ src/components/DemoHeader/DemoHeader.tsx
@@ -1,7 +1,7 @@
 export function DemoHeader() {
   return (
     <header className="header">
-      <h1 className="header__title">Glassmorphism StopWatch</h1>
+      <h1 className="header__title">Glassmorphism Stopwatch</h1>
       <div className="header__btns">
         <a
           href="https://github.com/nat-davydova/glassmorphism-stopwatch"
