c79392d61621064b8000dd57d92dd14d59799569
nat-davydova <natti.davydova@gmail.com>
2023-12-13 16:07:24 +0300

    feat: add basic stopwatch container styles

--- src/components/Stopwatch/StopwatchContainer/StopwatchContainer.tsx
+++ src/components/Stopwatch/StopwatchContainer/StopwatchContainer.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 
+import styles from "./StopwatchContainer.module.css";
+
 interface IStopwatchContainerProps {
   children: ReactNode | ReactNode[];
 }
 
 export function StopwatchContainer({ children }: IStopwatchContainerProps) {
-  return <div>{children}</div>;
+  return <div className={styles.stopwatchContainer}>{children}</div>;
 }
