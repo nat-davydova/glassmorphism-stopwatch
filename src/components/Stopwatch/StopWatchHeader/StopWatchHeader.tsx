c035add94f71c22d56977c42caadc7d532ecece5
nat-davydova <natti.davydova@gmail.com>
2023-12-18 19:38:48 +0300

    feat: add title styles

--- src/components/Stopwatch/StopWatchHeader/StopWatchHeader.tsx
+++ src/components/Stopwatch/StopWatchHeader/StopWatchHeader.tsx
@@ -1,8 +1,10 @@
+import styles from "./StopWatchHeader.module.css";
+
 export function StopWatchHeader() {
   return (
-    <div className="stopWatchHeader">
-      <h2 className="title">The Last Stopwatch You Ever Need</h2>
-      <p className="description">Maximum duration - 2 minutes</p>
+    <div className={styles.stopWatchHeader}>
+      <h2 className={styles.title}>The Last Stopwatch You Ever Need</h2>
+      <p className={styles.description}>Maximum duration - 2 minutes</p>
     </div>
   );
 }
