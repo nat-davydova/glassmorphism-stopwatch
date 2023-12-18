728171f473f86ca34cafe8f54aca57495f4f6b1a
nat-davydova <natti.davydova@gmail.com>
2023-12-18 19:28:19 +0300

    feat: add title structure

--- src/components/Stopwatch/StopWatchHeader/StopWatchHeader.tsx
+++ src/components/Stopwatch/StopWatchHeader/StopWatchHeader.tsx
@@ -1,3 +1,8 @@
 export function StopWatchHeader() {
-  return <div>HEYYYY</div>;
+  return (
+    <div className="stopWatchHeader">
+      <h2 className="title">The Last Stopwatch You Ever Need</h2>
+      <p className="description">Maximum duration - 2 minutes</p>
+    </div>
+  );
 }
