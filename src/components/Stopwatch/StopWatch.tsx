7bc4d6d0916cf3b0a28b76746a292940a0ce6bf2
nat-davydova <natti.davydova@gmail.com>
2023-12-18 19:47:29 +0300

    feat: add basic timer component

--- src/components/Stopwatch/StopWatch.tsx
+++ src/components/Stopwatch/StopWatch.tsx
@@ -1,10 +1,12 @@
 import { StopWatchHeader } from "./StopWatchHeader";
+import { StopWatchTimer } from "./StopWatchTimer";
 import { StopwatchContainer } from "./StopwatchContainer";
 
 export function StopWatch() {
   return (
     <StopwatchContainer>
       <StopWatchHeader />
+      <StopWatchTimer />
     </StopwatchContainer>
   );
 }
---
+++ src/components/Stopwatch/StopWatchTimer/index.ts
@@ -0,0 +1,1 @@
+export { StopWatchTimer } from "./StopWatchTimer";
