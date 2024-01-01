b6446ae70282d7b2cc2d6d2ddf1736bc37d081e7
nat-davydova <natti.davydova@gmail.com>
2024-01-01 13:54:53 +0300

    feat: add basic timer layout styles

--- src/components/Stopwatch/StopWatchTimer/StopWatchTimer.tsx
+++ src/components/Stopwatch/StopWatchTimer/StopWatchTimer.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 export function StopWatchTimer() {
   return (
     <div className={styles.timer}>
-      <div>15</div>
+      <div>15</div>:<div>15</div>:<div>15</div>
     </div>
   );
 }
