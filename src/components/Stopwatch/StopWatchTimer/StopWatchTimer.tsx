e8e0bb07de99c2e60ec314e50dcfddc132b9cfd3
nat-davydova <natti.davydova@gmail.com>
2024-01-02 13:35:57 +0300

    feat: add basic start handler

--- src/components/Stopwatch/StopWatchTimer/StopWatchTimer.tsx
+++ src/components/Stopwatch/StopWatchTimer/StopWatchTimer.tsx
@@ -4,17 +4,21 @@ import { TimerCounter } from "./TimerCounter";
 import styles from "./styles.module.css";
 
 export function StopWatchTimer() {
+  function handleStart() {
+    console.log("start");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.timer}>
         <TimerCounter value={2} />:<TimerCounter value={10} />:
-        <TimerCounter value={15} />
+        <TimerCounter value={155} />
       </div>
       <div className={styles.buttons}>
         <Button
           content="Start"
           classname={styles.start}
-          onClick={() => console.log("wow")}
+          onClick={handleStart}
         />
         <Button
           content="Stop"
